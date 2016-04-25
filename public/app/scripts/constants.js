53ad6cae94a7c508901a4a88f03794973ce10554
Support ERASME <support@erasme.org>
2016-04-25 16:32:19 +0200

    travaux liés aux déploiement de l'app

--- public/app/scripts/constants.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('quizsApp')
-.constant('LACLASSE_PATH', 'https://v3dev.laclasse.com')
-.constant('APP_PATH', 'http://localhost:9292/quiz');
-// .constant('APP_PATH', 'http://192.168.10.2:9292/quiz');
